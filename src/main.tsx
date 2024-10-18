553d54b5bea7daec1f33b764dba11d15fcee7e3f
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-18 22:55:04 +0200

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './pages/HomePage'
+import { StrictMode } from 'react';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './index.css';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import CvPage from './pages/CvPage';
+import HomePage from './pages/HomePage';
+import PortfolioPage from './pages/PortfolioPage';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<HomePage />} /> 
+      <Route path='about' element={<AboutPage />} />
+      <Route path='portfolio' element={<PortfolioPage />} />
+      <Route path='cv' element={<CvPage />} />
+      <Route path='contact' element={<ContactPage />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
