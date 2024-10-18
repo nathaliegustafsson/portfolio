47391a00447b9b5571faad91ae41b2603c468286
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-19 00:22:33 +0200

    add content and styling to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,66 @@
 import styled from "styled-components";
 
 function Header() {
+    const headerLinks = [
+        { text: 'About', href: 'about'},
+        { text: 'Portoflio', href: 'portfolio'},
+        { text: 'CV', href: 'cv'},
+        { text: 'Contact', href: 'contact'}
+    ]
+
     return (
-        <Container>
-            <LogoImg src="/initials-logo.png" alt="initials-logo" />
-        </Container>
+        <BigContainer>
+            <Container>
+                <a href="/">
+                    <LogoImg src="/initials-logo.png" alt="initials-logo" />
+                </a>
+                <LinksContainer>
+                {headerLinks.map((link) => (
+                        <HeaderLink key={link.text} href={link.href}>
+                            {link.text}
+                        </HeaderLink>
+                    ))}
+                </LinksContainer>
+            </Container>
+        </BigContainer>
     )
 }
 
+
 // Styling
+const BigContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
 const Container = styled.div`
-    background-color: pink;
+    width: 100%;
+    max-width: 1200px;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const LogoImg = styled.img`
     height: 50px;
+`;
+
+const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: auto;
+    gap: 50px;
+`;
+
+const HeaderLink = styled.a`
+    font-family: "Montserrat";
+    text-decoration: none;
+    color: black;
+
+    &:hover {
+        text-decoration: underline;
+        text-decoration-thickness: 0.01rem;
+        text-underline-offset: 0.3rem;
+    }  
 `;
 
 export default Header;
