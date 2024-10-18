2056db1ef61cef1ce92bf1f28d2ee9ae852dc61b
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-18 22:24:32 +0200

    add header to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import Header from './components/Header.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>,
 )
