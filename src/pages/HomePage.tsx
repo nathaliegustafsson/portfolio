9fe50e371a55b0472ca8bdfb1220582e82e623f4
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-22 22:24:19 +0200

    homePage styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,36 @@
+import styled from "styled-components";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Home from "../components/Home";
 
 function HomePage() {
     return (
-        <div>
-        <header>
-            <Header />
-        </header>
-        <main>
-            <Home />
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-        </div>
+        <HomePageStyling>
+            <header>
+                <Header />
+            </header>
+            <MainContent>
+                <Home />
+            </MainContent>
+            <footer>
+                <Footer />
+            </footer>
+        </HomePageStyling>
     )
 }
+
+// Styling
+const HomePageStyling = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+    display: flex;  
+    justify-content: center;
+    flex-direction: column;
+    flex: 1;
+`;
 
 export default HomePage;
