2a38e4a730c6fd6a7443556fdf7bf64465ee0392
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-22 21:54:41 +0200

    add content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,34 @@
+import styled from "styled-components";
+
 function Footer() {
     return (
-        <h1>hiiiiii footer</h1>
+        <Container>
+            <IconContainer>
+                <Icon src="/github-icon.svg" alt="github-icon" />
+                <Icon src="/linkedin-icon.svg" alt="linkedin-icon" />
+            </IconContainer>
+        </Container>
     )
 }
+
+// Styling
+const Container = styled.div`
+    background-color: pink;
+    border-top: 2px solid black;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const IconContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+`;
+
+const Icon = styled.img`
+    height: 45px;
+`;
 
 export default Footer;
