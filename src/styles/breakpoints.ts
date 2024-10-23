9d33a0ccee17e8b4f50ff5617828155417606f2e
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-24 00:05:08 +0200

    create file for breakpoints

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,12 @@
+const sizes = {
+    mobile: '480px',
+    tablet: '768px',
+    laptop: '1024px',
+};
+
+export const media = {
+    mobile: `(max-width: ${sizes.mobile})`,
+    tablet: `(max-width: ${sizes.tablet})`,
+    laptop: `(max-width: ${sizes.laptop})`,
+};
+
