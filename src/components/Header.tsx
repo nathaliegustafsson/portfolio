5de6735648c506bbc489033d109e8e524d73f279
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-29 20:25:52 +0100

    add hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { media } from "../styles/breakpoints";
 
 function Header() {
+    const [isMenuOpen, setMenuOpen] = useState(false)
+
     const headerLinks = [
         { text: 'About', href: 'about'},
         { text: 'Portoflio', href: 'portfolio'},
         { text: 'CV', href: 'cv'},
         { text: 'Contact', href: 'contact'}
     ]
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
 
     return (
         <BigContainer>
@@ -21,17 +29,26 @@ function Header() {
                         </HeaderLink>
                     ))}
                 </LinksContainer>
+                <HamburgerIcon src="/hamburger-icon.svg" alt="hamburger-icon" onClick={toggleMenu}/>
+                {isMenuOpen && (
+                    <MobileMenu>
+                        {headerLinks.map((link) => (
+                            <HeaderLink key={link.text} href={link.href}>
+                                {link.text}
+                            </HeaderLink>
+                        ))}
+                    </MobileMenu>
+                )}
             </Container>
         </BigContainer>
     )
 }
 
-
 // Styling
 const BigContainer = styled.div`
     display: flex;
     justify-content: center;
-    margin-top: 10px;
+    margin: 10px 10px 0 10px;
 `;
 
 const Container = styled.div`
@@ -48,14 +65,22 @@ const LogoLink = styled.a`
 `;
 
 const LogoImg = styled.img`
-    height: 50px;
+    height: 40px;
+
+    @media ${media.mobile} {
+        height: 50px;
+    }
 `;
 
 const LinksContainer = styled.div`
-    display: flex;
+    display: none;
     align-items: center;
     width: auto;
     gap: 50px;
+
+    @media ${media.tablet} {
+        display: flex;
+    }
 `;
 
 const HeaderLink = styled.a`
@@ -68,6 +93,30 @@ const HeaderLink = styled.a`
         text-decoration-thickness: 0.01rem;
         text-underline-offset: 0.3rem;
     }  
+`;
+
+const HamburgerIcon = styled.img`
+    display: flex;
+    width: 20px;
+
+    @media ${media.tablet} {
+        display: none;
+    }
+`;
+
+const MobileMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10px 15px 10px 15px;
+    gap: 10px;
+    font-size: 12px;
+    font-weight: 400;
+    position: absolute;
+    top: 50px;
+    right: 10px;
+    background: white;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    z-index: 10;
 `;
 
 export default Header;
