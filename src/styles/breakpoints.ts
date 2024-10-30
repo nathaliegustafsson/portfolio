4c16b27a60c4619ef8c196780154b4c678d169cb
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-30 22:12:40 +0100

    add desktopXL

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -2,11 +2,13 @@ const sizes = {
     mobile: '480px',
     tablet: '768px',
     desktop: '1024px',
+    desktopXL: '1600px',
 };
 
 export const media = {
     mobile: `(min-width: ${sizes.mobile})`,
     tablet: `(min-width: ${sizes.tablet})`,
     desktop: `(min-width: ${sizes.desktop})`,
+    desktopXL: `(min-width: ${sizes.desktopXL})`,
 };
 
