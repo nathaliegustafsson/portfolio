9ebf8357feadc088490ad681cf3c65d13a3e26f3
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-30 22:23:10 +0100

    styling

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,32 @@
+import styled from "styled-components";
+import { media } from "../styles/breakpoints";
+
 function Portfolio() {
     return (
-        <h1>hiiiiii Portfolio</h1>
+        <Title>Portfolio</Title>
     )
 }
+
+// Styling
+const Title = styled.h1`
+    display: inline-block;  
+    font-family: 'NewYork';
+    font-size: 55px;
+    font-weight: 100;
+    margin-bottom: 5px;
+    margin: 0;
+
+    @media ${media.mobile} {
+        font-size: 70px;
+    }
+
+    @media ${media.tablet} {
+        font-size: 80px;
+    }
+
+    @media ${media.desktopXL} {
+        font-size: 90px;
+    }
+`;
 
 export default Portfolio;
--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,21 +1,36 @@
+import styled from "styled-components";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Portfolio from "../components/Portfolio";
 
 function PortfolioPage() {
     return (
-        <div>
-        <header>
-            <Header />
-        </header>
-        <main>
-            <Portfolio />
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-        </div>
+        <PortfolioPageStyling>
+            <header>
+                <Header />
+            </header>
+            <MainContent>
+                <Portfolio />
+            </MainContent>
+            <footer>
+                <Footer />
+            </footer>
+        </PortfolioPageStyling>
     )
 }
+
+// Styling
+const PortfolioPageStyling = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+    display: flex;  
+    justify-content: center;
+    flex-direction: column;
+    flex: 1;
+`;
 
 export default PortfolioPage;
