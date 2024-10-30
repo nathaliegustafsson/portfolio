7a5e7aef92ebc85da3380642889291f1cba300f0
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-10-30 21:57:41 +0100

    change name

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,12 +1,12 @@
 const sizes = {
     mobile: '480px',
     tablet: '768px',
-    laptop: '1024px',
+    desktop: '1024px',
 };
 
 export const media = {
     mobile: `(min-width: ${sizes.mobile})`,
     tablet: `(min-width: ${sizes.tablet})`,
-    laptop: `(min-width: ${sizes.laptop})`,
+    desktop: `(min-width: ${sizes.desktop})`,
 };
 
