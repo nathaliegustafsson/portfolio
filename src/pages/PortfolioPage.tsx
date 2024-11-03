e3d04697691a0417876bac05968c84c009276738
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-11-03 21:16:48 +0100

    prettier

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,36 +1,35 @@
-import styled from "styled-components";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Portfolio from "../components/Portfolio";
+import styled from 'styled-components';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Portfolio from '../components/Portfolio';
 
 function PortfolioPage() {
-    return (
-        <PortfolioPageStyling>
-            <header>
-                <Header />
-            </header>
-            <MainContent>
-                <Portfolio />
-            </MainContent>
-            <footer>
-                <Footer />
-            </footer>
-        </PortfolioPageStyling>
-    )
+	return (
+		<PortfolioPageStyling>
+			<header>
+				<Header />
+			</header>
+			<MainContent>
+				<Portfolio />
+			</MainContent>
+			<footer>
+				<Footer />
+			</footer>
+		</PortfolioPageStyling>
+	);
 }
 
 // Styling
 const PortfolioPageStyling = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
 `;
 
 const MainContent = styled.main`
-    display: flex;  
-    justify-content: center;
-    flex-direction: column;
-    flex: 1;
+	display: flex;
+	flex-direction: column;
+	flex: 1;
 `;
 
 export default PortfolioPage;
