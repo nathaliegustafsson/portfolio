434a6364b3f9114bb10b0ef2628938fe5e3020a9
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-11-03 22:19:14 +0100

    add projectpage to router

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,34 @@
 import { StrictMode } from 'react';
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import './index.css';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import CvPage from './pages/CvPage';
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/PortfolioPage';
+import ProjectPage from './pages/ProjectPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route index element={<HomePage />} /> 
-      <Route path='about' element={<AboutPage />} />
-      <Route path='portfolio' element={<PortfolioPage />} />
-      <Route path='cv' element={<CvPage />} />
-      <Route path='contact' element={<ContactPage />} />
-    </Route>
-  )
-)
+	createRoutesFromElements(
+		<Route>
+			<Route index element={<HomePage />} />
+			<Route path="about" element={<AboutPage />} />
+			<Route path="portfolio" element={<PortfolioPage />} />
+			<Route path="portfolio/:projectId" element={<ProjectPage />} />
+			<Route path="cv" element={<CvPage />} />
+			<Route path="contact" element={<ContactPage />} />
+		</Route>
+	)
+);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
