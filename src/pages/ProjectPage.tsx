fff90f9cd9252f1844a4b6cf5208e1c3741b466d
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-11-04 23:15:33 +0100

    add styling

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,21 +1,42 @@
+import styled from 'styled-components';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
+import { media } from '../styles/breakpoints';
 
 function ProjectPage() {
 	return (
-		<div>
+		<ProjectPageStyling>
 			<header>
 				<Header />
 			</header>
-			<main>
+			<MainContent>
 				<Projects />
-			</main>
+			</MainContent>
 			<footer>
 				<Footer />
 			</footer>
-		</div>
+		</ProjectPageStyling>
 	);
 }
+
+// Styling
+const ProjectPageStyling = styled.div`
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	flex: 1;
+	overflow-y: auto;
+
+	@media ${media.desktopXL} {
+		justify-content: center;
+	}
+`;
 
 export default ProjectPage;
