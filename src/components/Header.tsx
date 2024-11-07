850a2e7e3f3d6a5ffa57fc1c429cc1542ca4e1fd
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-11-07 16:19:07 +0100

    change to click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,10 +32,10 @@ function Header() {
 			}
 		};
 
-		document.addEventListener('mousedown', handleClickOutside);
+		document.addEventListener('click', handleClickOutside);
 
 		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener('click', handleClickOutside);
 		};
 	}, []);
 
